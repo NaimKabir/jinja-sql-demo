458286fa3f1770b05dac910bd62337691f05e10d
NaimKabir <kabir.naim@gmail.com>
2021-08-14 18:00:19 -0700

    explode array for readability

--- models/example_funnel.sql
+++ models/example_funnel.sql
@@ -1,3 +1,9 @@
 {%- from 'volume_ratios.sql' import volume_ratios -%}
 
-{{ volume_ratios(['purchases', 'sessions', 'supportTickets'], 'marketing_pushes') }}
+{{ volume_ratios([
+        'purchases',
+        'sessions',
+        'supportTickets'
+        ],
+   'marketing_pushes')
+}}
